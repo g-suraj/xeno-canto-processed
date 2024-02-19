231d19647923a26e2c80d161c080bad542e35ad1
g-suraj <contactgsuraj@gmail.com>
2024-02-19 02:00:01 +0000

    Add a basic script to create a local DB

---
+++ join_and_pickle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3.11
+"""
+Taking the xeno-canto db, let's process the occurrences and
+multimedia files to construct a database of:
+
+* A set of birds that exist in the UK/Ireland,
+with an identifiable species key
+* For each bird, a set of birdsongs
+
+
+"""
+
+import csv
+import json
+import requests
+
+LOCATIONS = ["United Kingdom" or "Ireland"]
+
+SPECIES_INFO_FIELDS = [
+    "scientificName",
+    "higherClassification",
+    "kingdom",
+    "phylum",
+    "class",
+    "order",
+    "family",
+    "genus",
+    "vernacularName",
+    "speciesKey",
+]
+
+OCCURRENCE_FIELDS = [
+    "sex",
+    "fieldNotes",
+    "gbifID",
+    "level0Name",
+    "level1Name",
+    "level2Name",
+]
+
+multimedia_dict = {}
+data_dict = {}
+
+if __name__ == "__main__":
+    with open("multimedia.txt", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter="\t")
+        for row in reader:
+            if row["type"] == "Sound":
+                multimedia_dict[row["gbifID"]] = row
+
+    with open("occurrence.txt", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter="\t")
+        for row in reader:
+            if row["level0Name"] not in LOCATIONS:
+                continue
+
+            if not row["speciesKey"]:
+                continue
+
+            species_key = row["speciesKey"]
+            multimedia_match = multimedia_dict[row["gbifID"]]
+            this_occurrence = {
+                "__birdsong": multimedia_match["identifier"],
+                "__license": multimedia_match["license"],
+                "__rightsHolder": multimedia_match["rightsHolder"],
+                **{field: row[field] for field in OCCURRENCE_FIELDS},
+            }
+
+            if species_key not in data_dict:
+                data_dict[species_key] = {
+                    field: row[field] for field in SPECIES_INFO_FIELDS
+                }
+                try:
+                    response = requests.get(
+                        f"https://api.gbif.org/v1/species/{species_key}/media",
+                        timeout=5,
+                    )
+                    data_dict[species_key]["_pictures"] = [
+                        item["identifier"] for item in response.json()["results"]
+                    ]
+                except:  # pylint: disable=bare-except
+                    pass
+
+            if "_occurrences" in data_dict[species_key]:
+                data_dict[species_key]["_occurrences"].append(this_occurrence)
+            else:
+                data_dict[species_key]["_occurrences"] = [this_occurrence]
+
+    with open("pickled_data.json", "w+", encoding="utf-8") as file:
+        json.dump(list(data_dict.values()), file)
